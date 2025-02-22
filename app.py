553b77aa2f691a0a3be41713538a473629c9db61
madtechy <riteshraj22222@gmail.com>
2025-02-22 10:57:56 +0000

    app completed

---
+++ app.py
@@ -0,0 +1,35 @@
+from utlis import StockAPI
+import streamlit as st
+
+# Initialize the web app
+st.set_page_config(page_title="Stock Market Project", layout="wide")
+
+# Import the stock api client
+client = StockAPI()
+
+# Add a title to webpage
+st.title("Stock Market Project")
+
+# Add author name as subheader
+st.subheader("by Ritesh Singh")
+
+# Add company name as input
+company = st.text_input("Company Name :")
+
+# If company name is input then
+if company:
+    search = client.symbol_search(company)
+    option = st.selectbox("Company Symbol", options=search.keys())
+    selected = search[option]
+    st.success(f"Name : {selected[0]}")
+    st.success(f"Region : {selected[1]}")
+    st.success(f"Currency : {selected[2]}")
+
+    # Create a submit button
+    submit = st.button("Submit", type="primary")
+
+    # After pressing submit button
+    if submit:
+        df = client.get_daily_prices(option)
+        fig = client.plot_candlestick(df)
+        st.plotly_chart(fig)
